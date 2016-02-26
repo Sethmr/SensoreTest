b01940bcb4f424fdfbe879688c3615a4dcc728c2
Sethmr <seth.rininger@students.fhu.edu>
2016-02-25 19:39:38 -0500

    Everything seems functional

---
+++ app/src/main/java/com/example/seth/sensortest/CurrentWeather.java
@@ -0,0 +1,101 @@
+package com.example.seth.sensortest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Seth on 2/25/16.
+ */
+public class CurrentWeather {
+    private String[] mIcon = new String[7];
+    private long[] mTime = new long[7];
+    private int mTemperature;
+    private double[] mLowTemp = new double[7];
+    private double[] mHighTemp = new double[7];
+    private String mTimeZone;
+
+    public void setTime(long mTime, int index) {
+        this.mTime[index] = mTime;
+    }
+
+    public int getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double mTemperature) {
+        this.mTemperature = (int) Math.round(mTemperature);
+    }
+
+    public double[] getLowTemp() {
+        return mLowTemp;
+    }
+
+    public void setLowTemp(double mLowTemp, int index) {
+        this.mLowTemp[index] = mLowTemp;
+    }
+
+    public double[] getHighTemp() {
+        return mHighTemp;
+    }
+
+    public void setHighTemp(double mHighTemp, int index) {
+        this.mHighTemp[index] = mHighTemp;
+    }
+
+    public long[] getTime() {
+        return mTime;
+    }
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String mTimeZone) {
+        this.mTimeZone = mTimeZone;
+    }
+
+    public String[] getIcon() {
+
+        return mIcon;
+    }
+
+    public void setIcon(String mIcon, int index) {
+        this.mIcon[index] = mIcon;
+    }
+
+    public int getIconId(int index) {
+        int iconId = R.drawable.clear_day;
+
+        if (mIcon[index].equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        } else if (mIcon[index].equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        } else if (mIcon[index].equals("rain")) {
+            iconId = R.drawable.rain;
+        } else if (mIcon[index].equals("snow")) {
+            iconId = R.drawable.snow;
+        } else if (mIcon[index].equals("sleet")) {
+            iconId = R.drawable.sleet;
+        } else if (mIcon[index].equals("wind")) {
+            iconId = R.drawable.wind;
+        } else if (mIcon[index].equals("fog")) {
+            iconId = R.drawable.fog;
+        } else if (mIcon[index].equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        } else if (mIcon[index].equals("partly-cloudy-day")) {
+            iconId = R.drawable.cloudy_day;
+        } else if (mIcon[index].equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+        return iconId;
+    }
+
+    public String getFormattedTime(int index) {
+        SimpleDateFormat formatter = new SimpleDateFormat("EEE.");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTime = new Date(getTime()[index] * 1000);
+        String timeString = formatter.format(dateTime);
+        return timeString;
+    }
+}
--- app/src/main/java/com/example/seth/sensortest/SensorTestActivity.java
+++ app/src/main/java/com/example/seth/sensortest/SensorTestActivity.java
@@ -1,29 +1,72 @@
 package com.example.seth.sensortest;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.seth.sensortest.R;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.math.MathContext;
+import java.io.IOException;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class SensorTestActivity extends Activity implements SensorEventListener {
+
+    @Bind(R.id.day1) TextView textDay1;
+    @Bind(R.id.day2) TextView textDay2;
+    @Bind(R.id.day3) TextView textDay3;
+    @Bind(R.id.day4) TextView textDay4;
+    @Bind(R.id.day5) TextView textDay5;
+    @Bind(R.id.day6) TextView textDay6;
+    @Bind(R.id.day7) TextView textDay7;
+    @Bind(R.id.hiLow1) TextView textHiLow1;
+    @Bind(R.id.hiLow2) TextView textHiLow2;
+    @Bind(R.id.hiLow3) TextView textHiLow3;
+    @Bind(R.id.hiLow4) TextView textHiLow4;
+    @Bind(R.id.hiLow5) TextView textHiLow5;
+    @Bind(R.id.hiLow6) TextView textHiLow6;
+    @Bind(R.id.hiLow7) TextView textHiLow7;
+    @Bind(R.id.weather1) ImageView imageWeather1;
+    @Bind(R.id.weather2) ImageView imageWeather2;
+    @Bind(R.id.weather3) ImageView imageWeather3;
+    @Bind(R.id.weather4) ImageView imageWeather4;
+    @Bind(R.id.weather5) ImageView imageWeather5;
+    @Bind(R.id.weather6) ImageView imageWeather6;
+    @Bind(R.id.weather7) ImageView imageWeather7;
+
+    public static final String TAG = SensorTestActivity.class.getSimpleName();
+
     private SensorManager sensorManager;
     private boolean color = false;
     private View view;
     private long lastUpdate;
-    TextView textx, textz;
+    TextView textx, textz, textTemp;
     private int mAzimuth = 0;
 
     Button btnShowLocation;
@@ -31,18 +74,27 @@ public class SensorTestActivity extends Activity implements SensorEventListener 
 
     float[] orientation = new float[3];
     float[] rMat = new float[9];
+
+    String apiKey = "4b0324a6ed0c367123374acdc3476970";
+    double latitude = 37.8267;
+    double longitude = -122.423;
+    String forecastURL = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+    private CurrentWeather mCurrentWeather;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_sensor_test);
 
-        btnShowLocation = (Button) findViewById(R.id.button1);
+        ButterKnife.bind(this);
 
+        btnShowLocation = (Button) findViewById(R.id.button1);
         btnShowLocation.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -50,12 +102,15 @@ public class SensorTestActivity extends Activity implements SensorEventListener 
                 gps = new GPSTracker(SensorTestActivity.this);
 
                 if (gps.canGetLocation()) {
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
+                    latitude = gps.getLatitude();
+                    longitude = gps.getLongitude();
+                    forecastURL = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
 
                     String coords = (String) "Lat: " + latitude + "\nLong: "
                             + longitude;
                     setLabel(coords);
+
+                    getForecast();
                 } else {
                     gps.showSettingsAlert();
                 }
@@ -65,6 +120,7 @@ public class SensorTestActivity extends Activity implements SensorEventListener 
         // get textviews
         textx = (TextView) findViewById(R.id.azi);
         textz = (TextView) findViewById(R.id.zval);
+        textTemp = (TextView) findViewById(R.id.temp);
 
 
         view = findViewById(R.id.textView);
@@ -75,6 +131,104 @@ public class SensorTestActivity extends Activity implements SensorEventListener 
                 sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_NORMAL);
         lastUpdate = System.currentTimeMillis();
+    }
+
+    public void getForecast() {
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastURL)
+                    .build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
+                        if (response.isSuccessful()) {
+                            mCurrentWeather = getCurrentDetails(jsonData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+                                }
+                            });
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    } catch (JSONException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void updateDisplay() {
+        TextView[] textViewDays = {textDay1,textDay2,textDay3,textDay4,textDay5,textDay6,textDay7};
+        TextView[] textViewHiLows = {textHiLow1,textHiLow2,textHiLow3,textHiLow4,textHiLow5,textHiLow6,textHiLow7};
+        ImageView[] imageViewWeathers = {imageWeather1,imageWeather2,imageWeather3,imageWeather4,imageWeather5,imageWeather6,imageWeather7};
+        Drawable drawable;
+
+        textTemp.setText(mCurrentWeather.getTemperature() + "°");
+        for (int i = 0; i < 7; i++) {
+            textViewDays[i].setText(mCurrentWeather.getFormattedTime(i) + ".");
+            drawable = getResources().getDrawable(mCurrentWeather.getIconId(i));
+            imageViewWeathers[i].setImageDrawable(drawable);
+            textViewHiLows[i].setText(mCurrentWeather.getLowTemp()[i] + "°-" + mCurrentWeather.getHighTemp()[i] + "°");
+        }
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG, "From JSON: " + timezone);
+
+        JSONObject currently = forecast.getJSONObject("currently");
+        CurrentWeather currentWeather = new CurrentWeather();
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimeZone(timezone);
+
+        JSONObject daily = forecast.getJSONObject("daily");
+        JSONArray values = daily.getJSONArray("data");
+        for (int i = 0; i < 7; i++) {
+            JSONObject days = values.getJSONObject(i);
+            currentWeather.setIcon(days.getString("icon"), i);
+            currentWeather.setHighTemp(days.getDouble("temperatureMax"), i);
+            currentWeather.setLowTemp(days.getDouble("temperatureMin"), i);
+            currentWeather.setTime(days.getLong("time"), i);
+        }
+
+        Log.d(TAG, currentWeather.getFormattedTime(0));
+
+        return currentWeather;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
     public void setLabel(String s) {
